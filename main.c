1444e8e38561680d74aaadc81de0d38e132f00fb
Sathvik Saya <40954015+sathviksaya@users.noreply.github.com>
2019-09-10 16:53:11 +0530

    assignment_2

--- main.c
+++ main.c
@@ -1,81 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct Node
-{
-    int data;
-    struct Node *link;
-};
-typedef struct Node node;
-
-void push(node *ref, int data)
-{
-    node *temp = (node*)malloc(sizeof(node));
-    if(!temp)
-    {
-        printf("Memory is not allocated!! - \(stack overflow\)\n");
-    }
-    else
-    {
-        temp->data = data;
-        temp->link = ref->link;
-        ref->link = temp;
-    }
-}
-
-void peek(node *ref)
-{
-    printf("Top node has :- %d\n", ref->link->data);
-}
-
-void pop(node *ref)
-{
-    node *temp = ref->link;
-    if(ref->link == NULL)
-    {
-        printf("STACK IS EMPTY - \(stack underflow\)\n");
-    }
-    else
-    {
-        ref->link = temp->link;
-        printf("%d - node removed\n", temp->data);
-        free(temp);
-    }
-}
-
-void display(node *ref)
-{
-    node *temp = ref->link;
-    if(ref->link == NULL)
-    {
-        printf("STACK IS EMPTY\n");
-    }
-    else
-    {
-        while(temp != NULL)
-        {
-            printf("%d -> ", temp->data);
-            temp = temp->link;
-        }
-        printf("NULL\n");
-    }
-}
+#include<string.h>
 
 int main()
 {
-    node *head = (node*)malloc(sizeof(node));
-    head->link = NULL;
-    display(head);
-    push(head, 10);
-    display(head);
-    peek(head);
-    push(head, 20);
-    display(head);
-    peek(head);
-    push(head, 30);
-    display(head);
-    peek(head);
-    pop(head);
-    peek(head);
-    display(head);
+    int k, y = 0;
+    char s[100];
+    printf("Enter a string :- ");
+    scanf("%s", s);
+    for(int i = 0; i < strlen(s); i++)
+    {
+        k = (int)s[i];
+        if(k>=65 && k<=90)
+        {
+            s[i] = (char)(k+32);
+        }
+    }
+    for(int l = 0; l < strlen(s); l++)
+    {
+        char t[100];
+        int x = y;
+        while(s[l] == s[y])
+        {
+            t[y] = s[l];
+            ++y;
+        }
+        printf("%c%d", t[l], y-x);
+        l = y;
+    }
+    return 0;
 }
